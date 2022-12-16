7a8f1b937abafdf85698d9971700f46bde4afbeb
DEVTranVanTu <Dev.tutran@gmail.com>
2022-12-16 14:51:58 +0700

    update data

--- src/data/recommendNavigation.ts
+++ src/data/recommendNavigation.ts
@@ -1,32 +1,32 @@
 const recommendNavigation = [
   {
     title: 'Laptop sinh viên',
-    href: '/product/search/Laptop-Sinh-ViênVăn-Phòng-cat.62483e221c6b0cf4bb75b2cd',
+    href: '/product/search/Laptop-Sinh-ViênVăn-Phòng-cat.6394b125cc987b4ad646960e',
     menuComponent: 'MegaMenu2',
   },
   {
     title: 'Laptop Gaming',
-    href: '/product/search/Laptop-Gaming-cat.62483dff1c6b0cf4bb75b2c7',
+    href: '/product/search/Laptop-Gaming-cat.6394b0dbcc987b4ad64695fc',
     menuComponent: 'MegaMenu2',
   },
   {
     title: 'Laptop ThinkPad',
-    href: '/product/search/Laptop-Mỏng-Nhẹ-cat.62483e3c1c6b0cf4bb75b2d0',
+    href: '/product/search/Laptop-Mỏng-Nhẹ-cat.6394b138cc987b4ad6469614',
     menuComponent: 'MegaMenu2',
   },
   {
     title: 'Macbook',
-    href: '/product/search/Apple-cat.62483c4b1c6b0cf4bb75b299',
+    href: '/product/search/Apple-cat.6394af5fcc987b4ad646957e',
     menuComponent: 'MegaMenu2',
   },
   {
     title: 'Laptop đồ họa',
-    href: '/product/search/Laptop-Đồ-Họa-cat.62483e081c6b0cf4bb75b2ca',
+    href: '/product/search/Laptop-Đồ-Họa-cat.6394b0f1cc987b4ad6469602',
     menuComponent: 'MegaMenu2',
   },
   {
-    title: 'Laptop Mini',
-    href: '/product/search/Laptop-Mini-cat.62483e531c6b0cf4bb75b2d3',
+    title: 'Laptop cũ - giá rẻ',
+    href: '/product/search/Laptop-cu-gia-re-cat.639c231f0d33f3cac1dff522',
     menuComponent: 'MegaMenu2',
   },
 ]
